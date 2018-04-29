bc832b93b7684c6ed790b97e1adbed58e2e12984
Jason Turner <jason@emptycrate.com>
2018-04-29 16:57:51 -0600

    Reformatting of nana source code

--- nana/main.cpp
+++ nana/main.cpp
@@ -4,30 +4,30 @@
 
 int main()
 {
-    using namespace nana;
+  using namespace nana;
 
-    //Define a form.
-    form fm;
+  //Define a form.
+  form fm;
 
-    //Define a label and display a text.
-    label lab{fm, "Hello, <bold blue size=16>Nana C++ Library</>"};
-    lab.format(true);
+  //Define a label and display a text.
+  label lab{ fm, "Hello, <bold blue size=16>Nana C++ Library</>" };
+  lab.format(true);
 
-    //Define a button and answer the click event.
-    button btn{fm, "Quit"};
-    btn.events().click([&fm]{
-        fm.close();
-    });
+  //Define a button and answer the click event.
+  button btn{ fm, "Quit" };
+  btn.events().click([&fm] {
+    fm.close();
+  });
 
-    //Layout management
-    fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
-    fm["text"]<<lab;
-    fm["button"] << btn;
-    fm.collocate();
+  //Layout management
+  fm.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
+  fm["text"] << lab;
+  fm["button"] << btn;
+  fm.collocate();
 
-    //Show the form
-    fm.show();
+  //Show the form
+  fm.show();
 
-    //Start to event loop process, it blocks until the form is closed.
-    exec();
+  //Start to event loop process, it blocks until the form is closed.
+  exec();
 }
