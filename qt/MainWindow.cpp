55355a5309b9c952b915aa87e11f9513ea88ba29
alex <info@alexallmont.com>
2018-05-10 00:38:35 +0100

    Hook up Qt button to quit application on click

--- qt/MainWindow.cpp
+++ qt/MainWindow.cpp
@@ -5,8 +5,17 @@
 MainWindow::MainWindow(QWidget* parent) :
   QWidget(parent)
 {
+  // Create a button and hook up click event to close main window
+  auto button = new QPushButton("Click to close");
+  connect(
+    button, &QPushButton::clicked,
+    [this] {
+      close();
+    }
+  );
+
+  // Add button to this widget's layout
   auto layout = new QVBoxLayout;
-  auto button = new QPushButton("Click me");
   layout->addWidget(button);
   setLayout(layout);
 }
