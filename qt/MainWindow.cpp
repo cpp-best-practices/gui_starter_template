175f9621fce59dd2e5f9dbcdb5f7c63e749d9f41
alex <info@alexallmont.com>
2018-05-10 00:16:36 +0100

    Added push button to Qt main window

--- qt/MainWindow.cpp
+++ qt/MainWindow.cpp
@@ -1,6 +1,12 @@
 #include "MainWindow.h"
+#include <QBoxLayout>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget* parent) :
   QWidget(parent)
 {
+  auto layout = new QVBoxLayout;
+  auto button = new QPushButton("Click me");
+  layout->addWidget(button);
+  setLayout(layout);
 }
