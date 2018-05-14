7d5fea72efabf5055870c519cc95f00dafe7f720
Paul M. Bendixen <paulbendixen@gmail.com>
2018-05-14 19:41:31 +0200

    Qt example: Updated to follow advice from @alexallmont

--- qt/HelloQt.hpp
+++ qt/HelloQt.hpp
@@ -2,11 +2,11 @@
 
 class HelloQt : public QWidget
 {
-	Q_OBJECT // Very Important, this is all the magic that is needed
-	public:
-		explicit HelloQt( QWidget* parent = nullptr );
-		~HelloQt();
-	private slots:
-		void writeToDebug( void );
+  Q_OBJECT // Very Important, this is all the magic that is needed
+  public:
+    explicit HelloQt(QWidget* parent = nullptr);
+    ~HelloQt();
+  private slots:
+    void writeToDebug();
 
 };
--- qt/qtMain.cpp
+++ qt/qtMain.cpp
@@ -1,12 +1,12 @@
 #include "HelloQt.hpp"
 #include <QApplication>
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
-	QApplication app( argc, argv );
-	HelloQt dialog;
-	dialog.show();
+  QApplication app(argc, argv);
+  HelloQt dialog;
+  dialog.show();
 
-	return app.exec(); // this runs the main event loop and sees to it that cleanup is done
+  return app.exec(); // this runs the main event loop and sees to it that cleanup is done
 }
 
