94a7f0439098e019461bfb4eb4ce479921f023cd
Nis Meinert <nis.meinert@gmail.com>
2020-04-24 21:30:31 +0200

    ignore unusedFunction for LLVMFuzzerTestOneInput
    Ignore cppcheck warning 'unusedFunction' for `LLVMFuzzerTestOneInput`.
    To enable inline suppression the option `--inline-suppr` has to be added
    to the cppcheck invocation (cmake/StaticAnalyzers.cmake).

--- fuzz_test/fuzz_tester.cpp
+++ fuzz_test/fuzz_tester.cpp
@@ -14,6 +14,7 @@
 }
 
 // Fuzzer that attempts to invoke undefined behavior for signed integer overflow
+// cppcheck-suppress unusedFunction symbolName=LLVMFuzzerTestOneInput
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
   fmt::print("Value sum: {}, len{}\n", sum_values(Data,Size), Size);
