1f32530d2ddedb82d305b290e0374324c132ca58
giellamoswhard <45361334+giellamoswhard@users.noreply.github.com>
2020-04-26 16:25:42 +0200

    Update StandardProjectSettings.cmake
    Added colored diagnostic for Clang and Gcc

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -37,3 +37,11 @@ if(ENABLE_IPO)
 endif()
 
 
+if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+  add_compile_options(-fcolor-diagnostics)
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  add_compile_options(-fdiagnostics-color=always)
+else()
+  message(AUTHOR_WARNING "No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
+endif()
+
