215e4de279505ab1649c387ff2cd8dfae89645ea
aminya <aminyahyaabadi74@gmail.com>
2020-11-15 22:45:20 -0600

    Set VS_GLOBAL_RunCodeAnalysis to false

--- cmake/CodeAnalysis.cmake
+++ cmake/CodeAnalysis.cmake
@@ -6,7 +6,10 @@ if (CMAKE_GENERATOR MATCHES "Visual Studio")
     # Visual Studio Code Analysis
     foreach(target IN LISTS Targets)
       set_target_properties(${target} PROPERTIES
-          VS_GLOBAL_RunCodeAnalysis true
+          
+          # Run code analysis on build
+          # This is set to false delibarately. We already gave compiler warnings in CompilerWarnings file
+          VS_GLOBAL_RunCodeAnalysis false
 
           # MSVC
           VS_GLOBAL_EnableMicrosoftCodeAnalysis true
