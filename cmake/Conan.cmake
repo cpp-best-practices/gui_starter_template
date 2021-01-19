3046231bb8e1b3cfc87b7dce368327f0b4612952
David Dalcino <ddalcino@users.noreply.github.com>
2021-01-18 18:31:08 -0800

    Bump catch, fmt, spdlog

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -16,10 +16,10 @@ macro(run_conan)
   conan_cmake_run(
     REQUIRES
     ${CONAN_EXTRA_REQUIRES}
-    catch2/2.13.1
+    catch2/2.13.4
     docopt.cpp/0.6.3
-    fmt/7.0.3
-    spdlog/1.8.0
+    fmt/7.1.3
+    spdlog/1.8.2
     OPTIONS
     ${CONAN_EXTRA_OPTIONS}
     BASIC_SETUP
