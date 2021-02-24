eaa9e6a75b84c8b85e5524a7c0b3e9abaa5ee72b
Nis Meinert <nis.meinert@gmail.com>
2021-02-24 11:09:28 +0100

    Add assertions to fuzz test
    Add an example for using assertions within fuzz tests. This might be helpful since there is the caveat that when using assertions during fuzzing, one has to ensure that those assertions are not disabled, e.g., via `NDEBUG`. For larger projects one should factor out this testing for assertions but there are multiple ways to do this so inlining might be a good option for the sake of this exemplary file.

--- fuzz_test/fuzz_tester.cpp
+++ fuzz_test/fuzz_tester.cpp
@@ -17,6 +17,16 @@
 // cppcheck-suppress unusedFunction symbolName=LLVMFuzzerTestOneInput
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
-  fmt::print("Value sum: {}, len{}\n", sum_values(Data,Size), Size);
+  constexpr auto side_effect = 1;
+  static_assert([] {
+    int i = 0;
+    assert(i++ == 0);   // NOLINT
+    return i;
+  }() == side_effect, "Assertions are disabled but are required for fuzz testing");
+
+  const auto sum = sum_values(Data, Size);
+  fmt::print("Value sum: {}, len{}\n", sum, Size);
+  assert(sum >= 0); // NOLINT
+  
   return 0;
 }
