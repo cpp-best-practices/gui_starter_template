d34b787bd21aa047d2e0ebdafcc8618770e0b72b
Ralph J. Steinhagen <r.steinhagen@gsi.de>
2021-03-25 20:32:10 +0100

    add config to enable lgtm build
    for full syntax documentation see:  https://lgtm.com/help/lgtm/lgtm.yml-configuration-file

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -2,23 +2,25 @@ macro(run_conan)
   # Download automatically, you can also just copy the conan.cmake file
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake")
+    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.16.1/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake")
   endif()
 
   include(${CMAKE_BINARY_DIR}/conan.cmake)
 
   conan_add_remote(
-    NAME
-    bincrafters
-    URL
-    https://api.bintray.com/conan/bincrafters/public-conan)
+    NAME conan-center
+    URL https://api.bintray.com/conan/conan/conan-center)
+
+  conan_add_remote(
+    NAME bincrafters
+    URL https://api.bintray.com/conan/bincrafters/public-conan)
 
   conan_cmake_run(
     REQUIRES
     ${CONAN_EXTRA_REQUIRES}
     catch2/2.13.3
     docopt.cpp/0.6.2
-    fmt/6.2.0
+    fmt/6.2.1
     spdlog/1.5.0
     OPTIONS
     ${CONAN_EXTRA_OPTIONS}
