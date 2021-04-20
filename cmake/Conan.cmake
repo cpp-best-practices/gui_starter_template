f09af847b80ccb9462c1609e36a23abec858d640
Alexander Krimm <A.Krimm@gsi.de>
2021-04-20 11:21:39 +0200

    Update Conan.cmake to v0.16.1
    Also chage snippet according to conan cmake's
    [README](https://github.com/conan-io/cmake-conan/blob/develop/README.md).
    Mainly directly link to github's cdn and enable verification of the tls
    connection and the file hash.
    
    fixes: #135

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -2,7 +2,10 @@ macro(run_conan)
   # Download automatically, you can also just copy the conan.cmake file
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake")
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
+      "${CMAKE_BINARY_DIR}/conan.cmake"
+      EXPECTED_HASH SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
+      TLS_VERIFY ON)
   endif()
 
   include(${CMAKE_BINARY_DIR}/conan.cmake)
