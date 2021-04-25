efe7f7ca8930bc949b2580079c7757376605ae69
Vittorio Romeo <vittorio.romeo@outlook.com>
2021-04-25 22:57:21 +0100

    Add `-Wimplicit-fallthrough` to Clang warnings
    Clang doesn't enable this warning by default neither in `-Wall` nor `-Wextra`.

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -47,6 +47,7 @@ function(set_project_warnings project_name)
       -Wnull-dereference # warn if a null dereference is detected
       -Wdouble-promotion # warn if float is implicit promoted to double
       -Wformat=2 # warn on security issues around functions that format output (ie printf)
+      -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
   )
 
   if(WARNINGS_AS_ERRORS)
