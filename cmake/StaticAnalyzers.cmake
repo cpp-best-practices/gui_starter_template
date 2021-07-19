154a8fa8e342b46fcd8196e7c1b0ea210ca26d83
Arne Mertz <github@arne-mertz.de>
2021-07-19 18:33:44 +0200

    set warning-as-errors for static analyzers cond.
    pass warning-as-errors flags to static analyzers Cppcheck and clang-tidy conditionally depending
    on CMake option WARNINGS_AS_ERRORS
    
    sadly does not seem possible for iwyu which always fails so CMake
    ignores return codes

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -7,13 +7,15 @@ if(ENABLE_CPPCHECK)
   if(CPPCHECK)
     set(CMAKE_CXX_CPPCHECK
         ${CPPCHECK}
-        --error-exitcode=2
         --suppress=missingInclude
         --enable=all
         --inline-suppr
         --inconclusive
         -i
         ${CMAKE_SOURCE_DIR}/imgui/lib)
+    if(WARNINGS_AS_ERRORS)
+      list(APPEND CMAKE_CXX_CPPCHECK --error-exitcode=2)
+    endif()
   else()
     message(SEND_ERROR "cppcheck requested but executable not found")
   endif()
@@ -23,6 +25,9 @@ if(ENABLE_CLANG_TIDY)
   find_program(CLANGTIDY clang-tidy)
   if(CLANGTIDY)
     set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -extra-arg=-Wno-unknown-warning-option)
+    if(WARNINGS_AS_ERRORS)
+      list(APPEND CMAKE_CXX_CLANG_TIDY -warnings-as-errors=*)
+    endif()
   else()
     message(SEND_ERROR "clang-tidy requested but executable not found")
   endif()
