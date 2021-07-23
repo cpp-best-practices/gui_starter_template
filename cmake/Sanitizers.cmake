295dede0b3f4ed341fe7a24b0b71a81df7f7ee57
Arne Mertz <github@arne-mertz.de>
2021-07-23 08:16:24 +0200

    warn about need for instrumented libs when MSan is used with clang
    when clang is invoked with -fsanitize=memory, all libraries, including
    the standard library has to be built with that instrumentation to avoid
    false positives
    
    https://github.com/google/sanitizers/wiki/MemorySanitizerLibcxxHowTo

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -36,6 +36,7 @@ function(enable_sanitizers project_name)
 
     option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" FALSE)
     if(ENABLE_SANITIZER_MEMORY AND CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+      message(WARNING "Memory sanitizer reports false positives when running against libraries that are not MSan-instrumented (including libc++)")
       if("address" IN_LIST SANITIZERS
          OR "thread" IN_LIST SANITIZERS
          OR "leak" IN_LIST SANITIZERS)
