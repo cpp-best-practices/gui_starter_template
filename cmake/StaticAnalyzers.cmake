b4200b3404f4f0a1a573cde02f218985efe28c70
aminya <aminyahyaabadi74@gmail.com>
2021-08-02 18:12:37 -0500

    Move the code to StaticAnalyzers.cmake

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -41,3 +41,22 @@ if(ENABLE_INCLUDE_WHAT_YOU_USE)
     message(SEND_ERROR "include-what-you-use requested but executable not found")
   endif()
 endif()
+
+if (CMAKE_GENERATOR MATCHES "Visual Studio")
+    # Visual Studio Code Analysis
+    foreach(target IN LISTS Targets)
+      set_target_properties(${target} PROPERTIES
+
+          # Run code analysis on build
+          # This is set to false delibarately. We already gave compiler warnings in CompilerWarnings file
+          VS_GLOBAL_RunCodeAnalysis false
+
+          # MSVC
+          VS_GLOBAL_EnableMicrosoftCodeAnalysis true
+          VS_GLOBAL_CodeAnalysisRuleSet AllRules.ruleset  # See for other rulesets: C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Team Tools\Static Analysis Tools\Rule Sets\
+
+          # Clang tidy
+          VS_GLOBAL_EnableClangTidyCodeAnalysis true
+      )
+    endforeach()
+endif()
