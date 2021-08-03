f5133433426300ebece701549e0239e4b4e3dbe0
Arne Mertz <arnemertz@users.noreply.github.com>
2021-08-03 09:59:07 +0200

    Update cmake/Sanitizers.cmake
    Co-authored-by: Amin Yahyaabadi <aminyahyaabadi74@gmail.com>

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -36,7 +36,7 @@ function(enable_sanitizers project_name)
 
     option(ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" FALSE)
     if(ENABLE_SANITIZER_MEMORY AND CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-      message(WARNING "Memory sanitizer reports false positives when running against libraries that are not MSan-instrumented (including libc++)")
+      message(WARNING "Memory sanitizer requires all the code (including libc++) to be MSan-instrumented otherwise it reports false positives")
       if("address" IN_LIST SANITIZERS
          OR "thread" IN_LIST SANITIZERS
          OR "leak" IN_LIST SANITIZERS)
