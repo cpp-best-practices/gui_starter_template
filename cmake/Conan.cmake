40d2bd7a3c955cb3509fa05184eb28abec1d704d
Jason Turner <jason@emptycrate.com>
2021-08-24 15:17:44 -0600

    Modernize use of cmake_conan and upgrade packages
     * follow the example here https://github.com/conan-io/cmake-conan
       (modified to account for external conanfile.py)
     * use find_package instead of CONAN_PKG:: names
     * Add new cci remote
    
    Note that the sdl2 and imgui-sfml don't work for anonymous users at the
    moment

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,33 +1,38 @@
 macro(run_conan)
   # Download automatically, you can also just copy the conan.cmake file
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.16.1/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake")
+    message(
+      STATUS
+        "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(
+      DOWNLOAD
+      "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
+      "${CMAKE_BINARY_DIR}/conan.cmake"
+      EXPECTED_HASH
+        SHA256=396e16d0f5eabdc6a14afddbcfff62a54a7ee75c6da23f32f7a31bc85db23484
+      TLS_VERIFY ON)
   endif()
 
-  set (ENV{CONAN_REVISIONS_ENABLED} 1)
+  set(ENV{CONAN_REVISIONS_ENABLED} 1)
+  list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
+  list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
+
   include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-  conan_add_remote(
-    NAME
-    conan-center
-    URL
-    https://api.bintray.com/conan/conan/conan-center)
+  # conan_add_remote(NAME conan-center URL https://conan.bintray.com)
+  conan_add_remote(NAME cci URL https://center.conan.io INDEX 0)
 
-  conan_add_remote(
-    NAME
-    bincrafters
-    URL
-    https://bincrafters.jfrog.io/artifactory/api/conan/public-conan)
+  conan_cmake_autodetect(settings)
 
-  conan_cmake_run(
-    CONANFILE
-    conanfile.py
+  conan_cmake_install(
+    PATH_OR_REFERENCE
+    ${CMAKE_SOURCE_DIR}
+    BUILD
+    missing
     OPTIONS
     cpp_starter_use_imgui=${CPP_STARTER_USE_IMGUI}
     cpp_starter_use_sdl=${CPP_STARTER_USE_SDL}
-    BASIC_SETUP
-    CMAKE_TARGETS # individual targets to link to
-    BUILD
-    missing)
+    SETTINGS
+    ${settings})
+
 endmacro()
--- conanfile.py
+++ conanfile.py
@@ -11,12 +11,12 @@ class CppStarterProject(ConanFile):
     name = "CppStarterProject"
     version = "0.1"
     requires = (
-        "catch2/2.13.3",
+        "catch2/2.13.7",
         "docopt.cpp/0.6.2",
-        "fmt/6.2.1",
-        "spdlog/1.5.0",
+        "fmt/8.0.1",
+        "spdlog/1.9.2",
     )
-    generators = "cmake", "gcc", "txt"
+    generators = "cmake", "gcc", "txt", "cmake_find_package"
 
     def requirements(self):
         if self.options.cpp_starter_use_imgui == "ON":
