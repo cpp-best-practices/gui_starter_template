cff0e0632ae34141137267080e12871fae492481
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-22 14:49:37 -0500

    Remove unnecessary dynamism

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,6 @@ from conans import ConanFile
 
 
 class CppStarterProject(ConanFile):
-    # Note: options are copied from CMake boolean options.
-    # When turned off, CMake sometimes passes them as empty strings.
-    options = {
-        "cpp_starter_use_imgui": ["ON", "OFF", ""],
-        "cpp_starter_use_sdl": ["ON", "OFF", ""]
-    }
     name = "CppStarterProject"
     version = "0.1"
     requires = (
@@ -15,12 +9,7 @@ class CppStarterProject(ConanFile):
         "docopt.cpp/0.6.2",
         "fmt/8.0.1",
         "spdlog/1.9.2",
+        # "imgui-sfml/2.1@bincrafters/stable"
+        # "sdl2/2.0.10@bincrafters/stable"
     )
     generators = "cmake", "gcc", "txt", "cmake_find_package"
-
-    def requirements(self):
-        if self.options.cpp_starter_use_imgui == "ON":
-            self.requires("imgui-sfml/2.1@bincrafters/stable")
-        if self.options.cpp_starter_use_sdl == "ON":
-            self.requires("sdl2/2.0.10@bincrafters/stable")
-
