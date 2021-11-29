2428bab60b0c7bb2baf284fef1f89908bd2e08bc
Iason <Iason.Nikolas@gmail.com>
2021-11-29 10:48:51 +0000

    Update docopt version

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class CppStarterProject(ConanFile):
     version = "0.1"
     requires = (
         "catch2/2.13.7",
-        "docopt.cpp/0.6.2",
+        "docopt.cpp/0.6.3",
         "fmt/8.0.1",
         "spdlog/1.9.2",
     )
@@ -23,4 +23,3 @@ class CppStarterProject(ConanFile):
             self.requires("imgui-sfml/2.1@bincrafters/stable")
         if self.options.cpp_starter_use_sdl == "ON":
             self.requires("sdl2/2.0.10@bincrafters/stable")
-
